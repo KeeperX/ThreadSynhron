a276ed2ef51eb9f2e499b1f7dc6189968c79d8a3
KeeperX <KeeperX@yandex.ru>
2016-08-17 15:45:38 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@
  * Created by Admin on 17.08.2016.
  */
 public class Main {
-    private static int number=0;
+    private static int number;
     public static void main (String[] arg){
-     new Thread(new Worker()).start();
-    System.out.print(number);
+        Thread t1= new Thread(new Worker());
+        Thread t2= new Thread(new Worker());
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.print(number);
     }
-    public static void increaseNumber(){
+    public static synchronized void increaseNumber(){
         number++;
     }
 }
